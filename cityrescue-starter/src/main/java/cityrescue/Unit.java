f9f9142f3c0be757634bbd60989df0b528866fb8
jrf220 <jrf220@exeter.ac.uk>
2026-02-25 16:07:53 +0000

    Update Unit.java

--- cityrescue-starter/src/main/java/cityrescue/Unit.java
+++ cityrescue-starter/src/main/java/cityrescue/Unit.java
@@ -7,6 +7,7 @@ public abstract class Unit {
    private int numberOfUnits;
    private int[] currentPos;
    private String[] movementCandidates = {"NORTH", "EAST", "SOUTH", "WEST"}
+   private Object targetIncident;
 
    public Unit(){
       unitID = ++numberOfUnits;
@@ -19,12 +20,15 @@ public abstract class Unit {
    abstract String getCanRespondTo();
    abstract boolean canHandle(Incident type);
    abstract int getTicksToResolve();
+   public void setTargetIncident;
+   public Object getTargetIncident;
    public Object moveUnit(Object CityMap){
       // check manhattan distance to target
-      
+         // pythogras with target incident
       return CityMap
    }
 
 }
+
 
 
