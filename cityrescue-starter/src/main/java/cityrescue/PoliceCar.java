1eece8415a3e6804ad769061a72daf9bf60ff93a
jrf220 <jrf220@exeter.ac.uk>
2026-02-25 16:18:55 +0000

    Update PoliceCar.java

--- cityrescue-starter/src/main/java/cityrescue/PoliceCar.java
+++ cityrescue-starter/src/main/java/cityrescue/PoliceCar.java
@@ -7,31 +7,6 @@ class PoliceCar extends Unit {
         // this is a constructor method
         super();
     }
-
-    public String getUnitType(){
-        return unitType;
-    }
-    public String getUnitStatus(){
-        return unitStatus;
-    }
-    public String getCanRespondTo(){
-        return canRespondTo;
-    }
-    public int getUnitID(){
-        return unitID;
-    }
-    public void setUnitStatus(String unitStatus){
-        this.unitStatus = unitStatus;
-    }
-    public boolean canHandle(Incident type){
-        return (type.getIncidentType().equals(canRespondTo));
-    }
-    public int getTicksToResolve(){
-        return ticksToResolve;
-    }
-    public void moveUnit(){
-        
-    }
-
 }
+
 
