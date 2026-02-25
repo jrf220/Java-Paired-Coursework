f397b5d030998720f545e92b9fee9976c90898f4
AppsChan <ac1630@exeter.ac.uk>
2026-02-25 14:42:09 +0000

    Add files via upload

---
+++ cityrescue-starter/src/main/java/cityrescue/CityMap.java
@@ -0,0 +1,51 @@
+public class CityMap
+{
+    
+
+    private int[] gridSize;
+    private Object cityGrid;
+    private boolean[][] blocked;
+
+    public void setGridSize(int[] gridSizeIn)
+    {
+        gridSize = gridSizeIn;
+    }
+
+    public int[] getGridSize()
+    {
+        return gridSize;
+    }
+
+    public void setCityGrid(Object[][] cityGridIn)
+    {
+        cityGrid = cityGridIn
+    }
+
+    public Object[][] getCityGrid()
+    {
+        return cityGrid
+    }
+
+    public void addBlockedTile(int[] coords)
+    {
+        if (blocked[coords[0]][coords[1]] == false)
+            blocked[coords[0]][coords[1]] = true
+        else
+            throw new InputMismatchException("Roadcblock already placed at " + coords[0] +", " + coords[1])
+    }
+
+    public void setBlocked(boolean[][] blockedIn)
+    {
+        blocked = blockedIn
+    }
+
+    public boolean[][] getBlocked()
+    {
+        return blocked
+    }
+
+    public boolean legalMove()
+    {
+        return false
+    }
+}
