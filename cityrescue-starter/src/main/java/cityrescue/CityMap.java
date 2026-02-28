98847d0aa99272afb9ce7a428b67cd55128c4cd4
AppsChan <ac1630@exeter.ac.uk>
2026-02-28 20:56:22 +0000

    Enhance CityMap with station management and checks
    Added methods to manage stations and check surrounding tiles.

--- cityrescue-starter/src/main/java/cityrescue/CityMap.java
+++ cityrescue-starter/src/main/java/cityrescue/CityMap.java
@@ -49,13 +49,75 @@ public class CityMap
         }
     }
 
+    public void addStation(int[] coords, Station stationToBeAdded)
+    {
+        cityGrid[coords[0]][coords[1]] = stationToBeAdded;
+    }
+
+    public void removeStation(int[] coords)
+    {
+        cityGrid[coords[0]][coords[1]] = new Object();
+    }
+
     public boolean[][] getBlocked()
     {
         return blocked;
     }
 
+    public boolean isBlocked(int[] coords)
+    {
+        return blocked[coords[0]][coords[1]];
+    }
+
     public boolean legalMove()
     {
         return false;
+    }
+    
+    public boolean[] checkAround(int[] coords)
+    {
+        boolean[] unblockedDirectionList = new boolean[4]; // In the order: N, E, S, W
+
+        int tempx = coords[1] + 1;
+        if (tempx <= gridSize[1])
+        {
+            unblockedDirectionList[0] = isBlocked(new int[]{coords[0], tempx});
+        }
+        else
+        {
+            unblockedDirectionList[0] = true;
+        }
+        
+        tempx = coords[1] - 1;
+        if (tempx >= 0)
+        {
+            unblockedDirectionList[1] = isBlocked(new int[]{coords[0], tempx});
+        }
+        else
+        {
+            unblockedDirectionList[1] = true;
+        }
+
+        int tempy = coords[0] + 1;
+        if (tempy <= gridSize[0])
+        {
+            unblockedDirectionList[2] = isBlocked(new int[]{tempy, coords[1]});
+        }
+        else
+        {
+            unblockedDirectionList[2] = true;
+        }
+
+        tempy = coords[0] - 1;
+        if (tempy >= 0)
+        {
+            unblockedDirectionList[3] = isBlocked(new int[]{tempy, coords[1]});
+        }
+        else
+        {
+            unblockedDirectionList[3] = true;
+        }
+
+        return unblockedDirectionList;
     }
 }
