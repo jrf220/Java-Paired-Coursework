992a5bccc9f446dcab8490b9de0688408157ebef
jrf220 <jrf220@exeter.ac.uk>
2026-03-02 11:43:43 +0000

    Update Incident.java

--- cityrescue-starter/src/main/java/cityrescue/Incident.java
+++ cityrescue-starter/src/main/java/cityrescue/Incident.java
@@ -1,3 +1,11 @@
+/**
+* The Incident class represents the three types of incidents that can exist
+* in this city map, Fire, Medical and Crime, and how to handle these.
+*
+* @author Jacob Foot
+* @version 1.0
+* @since 2026
+*/
 public class Incident{
     private String incidentType;
     private int incidentID;
@@ -19,9 +27,32 @@ public class Incident{
     public void setIncidentStatus(String incidentStatus) {this.incidentStatus = incidentStatus;}
     public void setPosition(int[] position) {this.position = position;}
     
+    /**
+    * Cancels a reported or dispatched unit
+    *
+    */
     public void cancelIncident(){
-        if (incidentStatus == "REPORTED" || incidentStatus == "DISPATCHED"){
+        if (incidentStatus.equals("REPORTED") || incidentStatus.equals("DISPATCHED")){
             this.setIncidentStatus("CANCELLED");
         }
+    }
+
+    /**
+    * Checks which unit is closest to this incident
+    *
+    * @param units a list of all valid units
+    * @return the closest instance of the Unit class
+    */
+    public Unit closestUnit(Unit[] units){
+        Unit lowestUnit = units[0];
+        int lowestMan = 0;
+        for (int i = 0; i < units.length; i++){
+            int currentMan = units[i].getManhattanDistance(this.position);
+            if ((currentMan < lowestMan) && (units[i].canHandle(this))){
+                lowestMan = currentMan;
+                lowestUnit = units[i];
+                }
+        }
+        return lowestUnit;
     }
 }
