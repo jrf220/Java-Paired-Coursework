f2e38c87f2c1fc5881d273e41dd94e57599f6226
AppsChan <ac1630@exeter.ac.uk>
2026-03-03 15:42:28 +0000

    Add blockedCount to track blocked tiles
    Added blockedCount to track the number of blocked tiles. Updated methods to increment or decrement blockedCount when adding or removing blocked tiles.

--- cityrescue-starter/src/main/java/cityrescue/CityMap.java
+++ cityrescue-starter/src/main/java/cityrescue/CityMap.java
@@ -1,11 +1,19 @@
 package cityrescue;
 import cityrescue.exceptions.InvalidLocationException;
-
+/**
+* The CityMap class is a class used to keep track of tiles on the city that are blocked and also
+* managing the placement and removal of the blocks.
+* 
+* @author Appsharan Chandrarajan
+* @version 1.0
+* @since 2026
+*/
 public class CityMap
 {   
     //The grid uses a 0-based coordinate system. A location (x, y) is in bounds iff 0 ≤ x < width and 0 ≤ y < height.
     private int[] gridSize;
     private boolean[][] blocked;
+    static private int blockedCount = 0;
 
     public CityMap(int[] gridSizeIn)
     {
@@ -18,6 +26,15 @@ public class CityMap
         return gridSize;
     }
 
+    public int getBlockedCount()
+    {
+        return blockedCount;
+    }
+    
+    /**
+    * This method checks whether the coordinates entered are within the grid and returns true if
+    * they are and false if they are not.
+    */
     public boolean checkInGrid(int[] coords)
     {
         if (coords[0] >= gridSize[0] || coords[0] < 0)
@@ -30,18 +47,23 @@ public class CityMap
         }
         return true;
     }
-
+    
+    /**
+    * This adds a blocked tile to the grid and checks whether the coordinates are valid
+    * and the blockage is not placed on another.
+    */
     public void addBlockedTile(int[] coords) throws InvalidLocationException
     {
         if (checkInGrid(coords) == false)
         {
-            throw new InvalidLocationException("Roadcblock already placed at " + coords[0] +", " + coords[1]);
+            throw new InvalidLocationException("Coordinates not within the grid");
         }
         else
         {
             if (blocked[coords[1]][coords[0]] == false)
             {
                 blocked[coords[1]][coords[0]] = true;
+                blockedCount++;
             }
            else
             {
@@ -50,17 +72,22 @@ public class CityMap
         }
     }
 
+    /**
+    * This removes a blocked tile on the  grid and checks whether the coordinates are valid
+    * and when removing, you are not removing from something that is not there.
+    */
     public void removeBlockedTile(int[] coords) throws InvalidLocationException
     {
         if (checkInGrid(coords) == false)
         {
-            throw new InvalidLocationException("Roadblock already placed at " + coords[0] +", " + coords[1]);
+            throw new InvalidLocationException("Coordinates not within the grid");
         }
         else
         {
             if (blocked[coords[1]][coords[0]] == true)
             {
                 blocked[coords[1]][coords[0]] = false;
+                blockedCount--;
             }
             else
             {
@@ -69,16 +96,15 @@ public class CityMap
         }
     }
 
-    public boolean[][] getBlocked()
-    {
-        return blocked;
-    }
-
+    /**
+    * Checks whether the coordinates entered are within the grid, and if they are it will
+    * return whether there is a blockage placed there or not.
+    */
     public boolean isBlocked(int[] coords) throws InvalidLocationException
     {
         if (checkInGrid(coords) == false)
         {
-            throw new InvalidLocationException("Roadcblock already placed at " + coords[0] +", " + coords[1]);
+            throw new InvalidLocationException("Coordinates not within the grid.");
         }
         else
         {
@@ -86,11 +112,15 @@ public class CityMap
         }
     }
 
+    /**
+    * If the coordinates entered are within the grid, it checks the directions around the
+    * location to see if they can move N, E, S, W without moving of the grid or into a blockage.
+    */
     public boolean[] checkAround(int[] coords) throws InvalidLocationException
     {
         if (checkInGrid(coords) == false)
         {
-            throw new InvalidLocationException("Roadcblock already placed at " + coords[0] +", " + coords[1]);
+            throw new InvalidLocationException("Coordinates not within the grid");
         }
 
         else
