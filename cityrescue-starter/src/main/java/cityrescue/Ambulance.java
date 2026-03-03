028ae8331764c4ca49283426b2513bf4367f6717
jrf220 <jrf220@exeter.ac.uk>
2026-03-03 22:21:50 +0000

    Update Ambulance.java

--- cityrescue-starter/src/main/java/cityrescue/Ambulance.java
+++ cityrescue-starter/src/main/java/cityrescue/Ambulance.java
@@ -5,7 +5,7 @@ import cityrescue.enums.UnitType;
 class Ambulance extends Unit {
     private final UnitType unitType = UnitType.AMBULANCE;
     private final IncidentType canRespondTo = IncidentType.MEDICAL;
-    private final int ticksToResolve = 2;
+    private static final int ticksToResolve = 2;
 
     Ambulance(int homeStationId){
         // this is a constructor method
