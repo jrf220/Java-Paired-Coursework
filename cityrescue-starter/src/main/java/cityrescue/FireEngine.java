cd7afacc57f580125b5a7293322fe0f1779571ed
jrf220 <jrf220@exeter.ac.uk>
2026-03-03 22:22:05 +0000

    Update FireEngine.java

--- cityrescue-starter/src/main/java/cityrescue/FireEngine.java
+++ cityrescue-starter/src/main/java/cityrescue/FireEngine.java
@@ -5,7 +5,7 @@ import cityrescue.enums.UnitType;
 class FireEngine extends Unit {
     private final UnitType unitType = UnitType.FIRE_ENGINE;
     private final IncidentType canRespondTo = IncidentType.FIRE;
-    private final int ticksToResolve = 4;
+    private static final int ticksToResolve = 4;
 
     FireEngine(int homeStationId){
         // this is a constructor method
