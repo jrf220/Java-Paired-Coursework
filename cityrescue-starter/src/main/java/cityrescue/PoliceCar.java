1681e0061de99efd88c47552d342d8377b3c2e1c
jrf220 <jrf220@exeter.ac.uk>
2026-03-03 22:22:18 +0000

    Update PoliceCar.java

--- cityrescue-starter/src/main/java/cityrescue/PoliceCar.java
+++ cityrescue-starter/src/main/java/cityrescue/PoliceCar.java
@@ -5,7 +5,7 @@ import cityrescue.enums.UnitType;
 class PoliceCar extends Unit {
     private final UnitType unitType = UnitType.POLICE_CAR;
     private final IncidentType canRespondTo = IncidentType.CRIME;
-    private final int ticksToResolve = 3;
+    private static final int ticksToResolve = 3;
 
     PoliceCar(int homeStationId){
         // this is a constructor method
