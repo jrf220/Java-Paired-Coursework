9c4885812fb5a4b720f3df258281ffc94115e338
jrf220 <jrf220@exeter.ac.uk>
2026-03-04 16:34:39 +0000

    Update Station.java

--- cityrescue-starter/src/main/java/cityrescue/Station.java
+++ cityrescue-starter/src/main/java/cityrescue/Station.java
@@ -3,6 +3,7 @@ package cityrescue;
 import cityrescue.exceptions.CapacityExceededException;
 import cityrescue.exceptions.IDNotRecognisedException;
 import cityrescue.exceptions.InvalidCapacityException;
+
 /**
 * The Station Class is a class used to keep track of a particular stations status.
 * This includes its position, parking lot and the capcaity of it and whether it changes or not.
@@ -29,45 +30,19 @@ public class Station {
         position = inPosition;
     }
 
-    public int getStationID()
-    {
-        return stationID;
-    }
-
-    public String getStationName()
-    {
-        return stationName;
-    }
-
-    public int[] getPosition()
-    {
-        return position;
-    }
-
-    public static int getNumberOfStations()
-    {
-        return numberOfStations;
-    }
-
-    public int getCurrentStationCapacity()
-    {
-        return currentCapacity;
-    }
-
-    public int getMaxCapacity()
-    {
-        return maxCapacity;
-    }
+    public int getStationID() {return stationID;}
+    public String getStationName() {return stationName;}
+    public int[] getPosition() {return position;}
+    public static int getNumberOfStations() {return numberOfStations;}
+    public int getCurrentStationCapacity() {return currentCapacity;}
+    public int getMaxCapacity() {return maxCapacity;}
 
     /**
     * Checks if the Car park empty.
     * 
     * @return Returns a boolean value specifyin when the car park of a station is empty
     */
-    public boolean isEmpty()
-    {
-        return currentCapacity == 0;
-    }
+    public boolean isEmpty() {return currentCapacity == 0;}
 
     /**
     * Checks if the Car park is full or not using the Current Capacity and
@@ -75,31 +50,23 @@ public class Station {
     * 
     * @return This returns whether the car park is full or not
     */
-    public boolean carParkFull()
-    {
-        return currentCapacity == maxCapacity;
-    }
+    public boolean carParkFull() {return currentCapacity == maxCapacity;}
 
     /**
     * This method is here for when the capacity of a station is increased or decreased.
     * It adjusts the carPark accordingly.
     * 
     * @param inCapacity This takes in an integer that specifies the new capacity of the car park of a given station.
+    * @throws InvalidCapacityException is the capacity given is less than the current capacity.
     */
-    public void setStationCapacity(int inCapacity) throws InvalidCapacityException
-    {
-        if (currentCapacity > inCapacity)
-        {
+    public void setStationCapacity(int inCapacity) throws InvalidCapacityException{
+        if (currentCapacity > inCapacity){
             throw new InvalidCapacityException("There are more units than parking spaces to be allocated");
-        }
-        else
-        {
+        } else {
             //This is copying over all the units to the bigger car park
             maxCapacity = inCapacity;
-
             Unit[] unitTempCarPark = new Unit[inCapacity];
-            for (int i = 0; i < carPark.length; i++)
-            {
+            for (int i = 0; i < carPark.length; i++){
                 unitTempCarPark[i] = carPark[i];
             }
             carPark = unitTempCarPark;
@@ -111,22 +78,16 @@ public class Station {
     * If the car park is full then it throws an excpetion.
     * 
     * @param unitToBeAdded Takes in the unit to be added to a given station.
+    * @throws CapacityExceededException if the unit does not fit in this station.
     */
-    public void addUnit(Unit unitToBeAdded) throws CapacityExceededException
-    {   
-        if (carParkFull() == true)
-        {
+    public void addUnit(Unit unitToBeAdded) throws CapacityExceededException{   
+        if (carParkFull() == true){
             throw new CapacityExceededException("No more units can be added to this station");
-        }
-        else
-        {
+        } else {
             currentCapacity++;
-        
-            for (int i = 0; i < carPark.length; i++)
-            {
+            for (int i = 0; i < carPark.length; i++){
                 //Adds the new unit in the empty space in the carPark array.
-                if (carPark[i] == null)
-                {
+                if (carPark[i] == null){
                     carPark[i] = unitToBeAdded;
                 }
             }
@@ -138,25 +99,20 @@ public class Station {
     * it will throw an exception.
     * 
     * @param unitIDtoBeRemoved Takes in an integer that is the unit id of the unit to be removed from the station.
+    * @throws IDNotRecognisedException if the unit ID does not exist in this station.
     */
-    public void removeUnit(int unitIDtoBeRemoved) throws IDNotRecognisedException
-    {
+    public void removeUnit(int unitIDtoBeRemoved) throws IDNotRecognisedException{
         boolean untiRemoved = false;
-
-        for (int i = 0; i < carPark.length; i++)
-        {
-            if (carPark[i].getUnitID() == unitIDtoBeRemoved)
-            {
+        for (int i = 0; i < carPark.length; i++){
+            if (carPark[i].getUnitID() == unitIDtoBeRemoved){
                 carPark[i] = null;
                 untiRemoved = true;
                 break;
             }
         }
-        if (untiRemoved == false)
-        {
+        if (untiRemoved == false){
             throw new IDNotRecognisedException("The unit was not present in the station car park");
-        }
-        else{
+        } else {
             currentCapacity--;
         }
     }
